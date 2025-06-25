eaadb33ee34418fa6b67a9ec0d205ca8ee5d8bd0
Sabina <meherremovasabina20@gmail.com>
2025-06-25 21:07:28 +0400

    fix sabina component

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import { Route, Routes } from 'react-router'
 import MainPage from './pages/mainPage/MainPage'
 import Messages from './pages/messages/Messages'
 import NotFoundPage from './pages/notFoundPage/NotFoundPage'
+import Sabina from './components/sabina/Sabina'
 
 function App() {
 
   return (
     <>
+    <Sabina/>
 
       <Routes>
         <Route path="/" element={<Layout />} >
